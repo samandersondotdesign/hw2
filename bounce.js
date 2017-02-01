cf99e1b265c30c482bf94ecf3f5ad9b8e373e96d
samandersondotdesign <samuelanderson@me.com>
2017-02-01 08:11:11 -0800

    Update bounce.js

--- bounce.js
+++ bounce.js
@@ -1,4 +1,4 @@
-var posX = 0;
+var posX = 100;
 var speed = 5;
 var left = -5;
 function setup() {
@@ -12,6 +12,9 @@ function draw() {
 	
 if(posX > 380) {
 	speed = speed - 5;
+}
+	if(posX < 20) {
+	speed = speed + 5;
 }
 	posX = posX + speed;
 	
